5a7576bdca779193135394c0539da8968fa55565
yatnallivikas <yatnallivikas@gmail.com>
2026-02-20 06:59:34 +0000

    chore: fix terraform project structure

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,14 +1,1 @@
-// Placeholder module describing how networking would be modeled.
-locals {
-  vpc_id = "vpc-PLACEHOLDER"
-  public_subnet_ids = [
-    "subnet-public-a",
-    "subnet-public-b"
-  ]
-  private_subnet_ids = [
-    "subnet-private-a",
-    "subnet-private-b"
-  ]
-}
-
-# TODO: Implement aws_vpc, aws_subnet, and related resources.
+# VPC module placeholder
