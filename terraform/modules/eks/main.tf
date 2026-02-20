b995b290ceefc0783ccd726c23d23d68b94d845f
Rahul Keluskar <rahul.doit53@gmail.com>
2026-02-20 12:50:49 +0530

    feat: fix terraform formatting

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,6 +1,6 @@
 // Placeholder module representing managed Kubernetes control plane.
 locals {
-  cluster_name = "eks-placeholder-cluster"
+  cluster_name  = "eks-placeholder-cluster"
   node_role_arn = "arn:aws:iam::123456789012:role/TODO-node-role"
 }
 
