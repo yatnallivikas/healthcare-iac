4093eccd1b7a84967291a7c2d2571fb0a91cd48b
yatnallivikas <yatnallivikas@gmail.com>
2026-02-20 18:42:24 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket  = "vikas-healthcare-tf-state-2026-xyz123"
-    key     = "infra/terraform.tfstate"
-    region  = "ap-south-1"
-    encrypt = true
+    bucket         = "vikas-healthcare-tf-state-2026-xyz123"
+    key            = "infra/terraform.tfstate"
+    region         = "ap-south-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table"
   }
 }
